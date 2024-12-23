0d8db530a5d34a66f46e732bffbb78bb602d98f3
Rita Larina <small.rita@gmail.com>
2024-12-23 19:59:38 +0200

    script removed from settings-modal.html file and moved to eventlisteners defined in settings-modal.js

--- src/renderer/modals/settings-modal.js
+++ src/renderer/modals/settings-modal.js
@@ -10,11 +10,27 @@ export function loadSettingsModal(modalFile) {
             const modal = modalContainer.querySelector('#settings-modal');
             modal.style.display = 'block';
 
-            // Add close behavior
-            modalContainer.querySelector('#close-settings').addEventListener('click', () => {
-                modal.style.display = 'none';
-                modalContainer.remove();
-            });
+            attachEventListeners(modalContainer, modal);
         })
         .catch(err => console.error('Error loading modal:', err));
+}
+
+function attachEventListeners(modalContainer, modal) {
+    const closeModalButton = modalContainer.querySelector('#close-settings');
+    if (closeModalButton) {
+        closeModalButton.addEventListener('click', () => {
+            modal.style.display = 'none';
+            modalContainer.remove();
+        });
+    }
+
+    const saveButton = modalContainer.querySelector('#save-settings');
+    if (saveButton) {
+        saveButton.addEventListener('click', () => {
+            const defaultLanguage = modalContainer.querySelector('#default-language').value;
+            localStorage.setItem('defaultLanguage', defaultLanguage);
+            modal.style.display = 'none';
+            modalContainer.remove();
+        });
+    }
 }
